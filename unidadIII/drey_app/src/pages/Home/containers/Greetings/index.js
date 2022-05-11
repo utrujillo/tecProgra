9680acd809cdbcbc52cf06a12b4f62b8af25bbc0
Xiada-GR <xgutiereez@gmail.com>
2022-05-11 14:40:35 -0500

    cambios xiada

--- unidadIII/drey_app/src/pages/Home/containers/Greetings/index.js
+++ unidadIII/drey_app/src/pages/Home/containers/Greetings/index.js
@@ -5,7 +5,7 @@ import classes from './Greetings.module.css'
 const Greetings = () => {
   return(
     <div className={classes.wrapper__greetings}>
-      <Text title='Bienvenido al sistema' css_styles='fs-36 grey-4' />
+      <Text title='Bienvenido al sistema Xiadi' css_styles='fs-36 grey-4' />
     </div>
   )
 }
