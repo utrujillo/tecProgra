a7f8bf700f7f688866d65ba0e1e08498100df8d2
tecnm-mx <samuel_ita95@outlook.com>
2022-05-14 13:01:17 -0500

    Derocando al rey

---
+++ unidadIII/drey_app/src/pages/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserInfo from './containers/UserInfo'
+import Form from './containers/Form'
+
+const About = () => {
+  return(
+    <>
+      <UserInfo />
+      <Form />
+    </>
+  )
+}
+
+export default About
---
+++ unidadIII/drey_app/src/pages/Home/containers/Greetings/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Text from '../../../../components/Text'
+import classes from './Greetings.module.css'
+
+const Greetings = () => {
+  return(
+    <div className={classes.wrapper__greetings}>
+      <Text title='Bienvenido al sistema' css_styles='fs-36 grey-4' />
+    </div>
+  )
+}
+
+export default Greetings
