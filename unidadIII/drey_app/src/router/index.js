fa4099bf908c2c3237e8458aeeee430654c631b3
Uziel Trujillo Colón <uziel.trujillo.mx@cloudappi.net>
2022-05-23 18:13:31 -0500

    Add Users module and create Hook 	new file:   src/App/App.js 	new file:   src/hooks/API/index.js 	new file:   src/pages/Users/index.js 	modified:   src/router/index.js

--- unidadIII/drey_app/src/router/index.js
+++ unidadIII/drey_app/src/router/index.js
@@ -1,16 +1,10 @@
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Users from '../pages/Users'
 const routes = [
-  {
-    name: 'Home',
-    path: '/',
-    component: Home
-  },
-  {
-    name: 'About',
-    path: '/about',
-    component: About
-  }
+  { name: 'Home', path: '/', component: Home },
+  { name: 'About', path: '/about', component: About },
+  { name: 'Users', path: '/users', component: Users }
 ]
 
 export default routes
