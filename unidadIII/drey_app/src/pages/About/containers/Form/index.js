742aa2d33f4c8b6b99e8bab2e3914d7f43b2624c
juansolid <buffonz185@gmail.com>
2022-05-30 11:27:10 -0500

    consumo

--- unidadIII/drey_app/src/pages/About/containers/Form/index.js
+++ unidadIII/drey_app/src/pages/About/containers/Form/index.js
@@ -2,50 +2,27 @@ import React, { useState } from 'react'
 import data from './fakeTableData.js'
 
 const Form = () => {
-
-  const [search, setSearch] = useState('') //parametros de busqueda
-  const inputChange = (e) => {
-    setSearch(e.target.value)
+  const [ search, setSearch ] = useState('')
+  const inputChange = ( e ) => {
+    setSearch( e.target.value )
   }
-
-  //const busqueda =  data.filter(item => item.ability.toLowerCase().indexOf(search.toLowerCase()) > -1)
-
-<<<<<<< HEAD
-
-
-  return (
-    <div>
-      <input type="text" placeholder="Search" value={search} onChange={inputChange} />
-      <input type="button" value="Ok"
-        onClick={() => { console.log(search) }} />
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Habilidad</th>
-            <th>Puntuacion</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {
-            data.filter(item => item.ability.toLowerCase().indexOf(search.toLowerCase()) > -1).map(item => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.ability}</td>
-                <td>{item.score}</td>
-              </tr>
-            ))
-          }
-
-        </tbody>
-      </table>
-    </div>
-=======
+  return(
+  <div>
+    <input type="text" placeholder="Search" value={search}  onChange={ inputChange } />
+    <input type="button" value="Ok"
+      onClick={ () => { console.log( search ) } } />
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Habilidad</th>
+          <th>Puntuacion</th>
+        </tr>
+      </thead>
       <tbody>
-        {
+      {
           data
-            .filter( item => item.score == search )
+          .filter(item => item.ability.toLowerCase().indexOf(search.toLowerCase()) > -1)
             .map( item => (
               <tr key={item.id}>
                 <td>{ item.id }</td>
@@ -53,13 +30,10 @@ const Form = () => {
                 <td>{ item.score }</td>
               </tr>
             ) )
-        }
-        
+        }     
       </tbody>
     </table>
   </div>
->>>>>>> fa4099bf908c2c3237e8458aeeee430654c631b3
   )
 }
-
 export default Form
--- unidadIII/drey_app/src/router/index.js
+++ unidadIII/drey_app/src/router/index.js
@@ -1,30 +1,12 @@
 import Home from '../pages/Home'
 import About from '../pages/About'
-<<<<<<< HEAD
 import Versus from '../pages/Versus'
-const routes = [
-  {
-    name: 'Home',
-    path: '/',
-    component: Home
-  },
-  {
-    name: 'About',
-    path: '/about',
-    component: About
-  },
-  {
-    name: 'Versus',
-    path: '/versus',
-    component:Versus
-  }
-=======
 import Users from '../pages/Users'
 const routes = [
   { name: 'Home', path: '/', component: Home },
   { name: 'About', path: '/about', component: About },
-  { name: 'Users', path: '/users', component: Users }
->>>>>>> fa4099bf908c2c3237e8458aeeee430654c631b3
+  { name: 'Users', path: '/users', component: Users },
+  { name: 'Versus', path: '/versus', component: Versus }
 ]
 
 export default routes
